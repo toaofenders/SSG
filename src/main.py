d5720e37622a3f4f934eee91b180814fbc538b51
Calvsie <toaofenders@gmail.com>
2025-04-09 10:11:49 -0500

    build

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from gencontent import (
 )
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
